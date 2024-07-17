9abc07b8085c371685167e3d4abf2951777ebb65
chenyulue <chenyulue@163.com>
2024-07-17 18:29:11 +0800

    app.py added

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+import streamlit as st
+
+
+pages24 = []
+for file in os.listdir("./ff24"):
+    name, _ = os.path.splitext(file)
+    pages24.append(
+        st.Page(f"ff24/{file}", title=name, url_path=f"ff24/{name}")
+    )
+
+pages = {
+    "": [st.Page("Home.py", title="Home")],
+    "Figure Friday 2024": pages24,
+}
+
+pg = st.navigation(pages)
+
+pg.run()
